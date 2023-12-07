a7691c5374f34b30a83ceef197db5abc836303c8
Prathi0410 <pilliprathimarani@my.unt.edu>
2023-12-07 15:09:09 -0600

    final1st

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('search-btn').addEventListener('click', function () {
+    // Implement search functionality here (this is where a back-end would be used in a real application)
+    alert('Searching...'); // Replace this with your search logic
+});
